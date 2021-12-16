7fe1f4abd6eb17660cd1508a81e238e9231e0d5f
jabberwokks41 <55454642+jabberwokks41@users.noreply.github.com>
2021-12-16 15:45:00 +0100

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,13 +1,13 @@
 #!/usr/bin/env groovy
 
-def call(String buidResult) {
+def call(String buildResult) {
   if (buildResult == "SUCCESS") {
     slackSend (color: "good", message: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
   }
   else if (buildResult == "FAILURE") {
     slackSend (color: "danger", message: "failed: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")  
   }
-  else if (buildResult == "unstable") {
+  else if (buildResult == "UNSTABLE") {
     slackSend (color: "warning", message: "unstable: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")  
   }
   else {
